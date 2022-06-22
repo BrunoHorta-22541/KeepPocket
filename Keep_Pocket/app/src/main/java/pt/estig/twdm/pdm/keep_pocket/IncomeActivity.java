c7c36c15247d6b1e4844703c6f2f3670f93a0ec0
BrunoHorta-22541 <22541@stu.ipbeja.pt>
2022-06-22 17:54:16 +0100

    botões do menu
    Co-Authored-By: DinisD <101331020+lought@users.noreply.github.com>
    Co-Authored-By: RodrigoSerafimIPB <101331174+RodrigoSerafimIPB@users.noreply.github.com>

--- Keep_Pocket/app/src/main/java/pt/estig/twdm/pdm/keep_pocket/IncomeActivity.java
+++ Keep_Pocket/app/src/main/java/pt/estig/twdm/pdm/keep_pocket/IncomeActivity.java
@@ -2,7 +2,9 @@ package pt.estig.twdm.pdm.keep_pocket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class IncomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class IncomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_income);
+    }
+    public void toExpenses(View view) {
+        Intent intent= new Intent (this, ExpensesActivity.class);
+        startActivity(intent);
+    }
+
+    public void toCategory(View view) {
+        Intent intent= new Intent (this, CategoryActivity.class);
+        startActivity(intent);
+    }
+
+    public void toIncome(View view) {
+        Intent intent= new Intent (this, IncomeActivity.class);
+        startActivity(intent);
+    }
+
+    public void toLimit(View view) {
+        Intent intent= new Intent (this, LimitActivity.class);
+        startActivity(intent);
+    }
+
+    public void tomain(View view) {
+        Intent intent= new Intent (this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- Keep_Pocket/app/src/main/java/pt/estig/twdm/pdm/keep_pocket/MainActivity.java
+++ Keep_Pocket/app/src/main/java/pt/estig/twdm/pdm/keep_pocket/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -61,5 +62,30 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void toExpenses(View view) {
+        Intent intent= new Intent (this, ExpensesActivity.class);
+        startActivity(intent);
+    }
+
+    public void toCategory(View view) {
+        Intent intent= new Intent (this, CategoryActivity.class);
+        startActivity(intent);
+    }
+
+    public void toIncome(View view) {
+        Intent intent= new Intent (this, IncomeActivity.class);
+        startActivity(intent);
+    }
+
+    public void toLimit(View view) {
+        Intent intent= new Intent (this, LimitActivity.class);
+        startActivity(intent);
+    }
+
+    public void tomain(View view) {
+        Intent intent= new Intent (this, MainActivity.class);
+        startActivity(intent);
     }
 }
